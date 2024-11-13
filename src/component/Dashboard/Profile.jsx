450d42ecacce71ce36211fe70a3ef01a9f6747bd
noseunghyeon <ro981009@gmail.com>
2024-11-13 18:15:03 +0900

    1113

---
+++ src/component/Dashboard/Profile.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="profile w-full">
+      <div className="block-head flex items-center justify-between ">
+        <span>logo</span>
+        <h2 className=" bg-gray-600">Crema Demo</h2>
+        <span>System Manager</span>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
